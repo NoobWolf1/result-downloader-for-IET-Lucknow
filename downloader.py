de9b266003781e3d57e3e829b965cff12405ed48
Malay Shukla <36460240+malayshukla21@users.noreply.github.com>
2018-02-16 03:14:08 +0530

    Added names of carrypapers as a row

--- downloader.py
+++ downloader.py
@@ -5,7 +5,6 @@ import time
 
 def get_page(roll):
 	params = {'g-recaptcha-response' : '' ,'roll': str(roll) , 'type' : 'R'}
-
 	r = ''
 	while r == '':
 		try :
@@ -20,46 +19,38 @@ def get_page(roll):
 	soup = BeautifulSoup(r.text, 'html5lib')
 	return soup
 
-def get_name(soup):
+def get_data(soup):
+
 	try :
 		name = soup.find_all('td')[1].get_text()
-		return name
-	except IndexError :
-		return None
-
-def get_SGPA(soup):
-	try :
 		SGPA = soup.find_all('td')[93].get_text()
 		SGPA = float(SGPA)
-		return SGPA
-	except IndexError:
-		return None
-
-def get_branch(soup) :
-	try :
 		branch = soup.find_all('td')[9].get_text()
 		branch = branch.split()[1:-3]     
-		branch = ' '.join(branch)  #collected branchname
-		return branch
-	except IndexError :
-		return None
+		branch = ' '.join(branch)
+		carry_papers = soup.find_all('td')[91].get_text()
+		carry_papers = carry_papers[:-1]
+
+		return name,SGPA,branch, carry_papers
+	except IndexError:
+		return None, None, None, None
 
 def result_downloader(first_roll,last_roll):
 	for i in range(first_roll,last_roll+1) :
 		new_page = get_page(i)
-		new_name = get_name(new_page)
-		new_SGPA = get_SGPA(new_page)
-		new_branch = get_branch(new_page)
+		new_name, new_SGPA, new_branch, new_carry_papers = get_data(new_page)
 		new_roll_number = i
+		new_carry_papers 
 
 		if new_name is None and new_SGPA is None:
 			continue
 		else :
 			cur.execute('SELECT Names FROM Results WHERE  Names = ? ', (new_name, ) )
 			row = cur.fetchone()
-			cur.execute(''' INSERT INTO Results (Rollnumber,Names,SGPA,Branch) VALUES (?,?,?,?) ''', (new_roll_number,new_name,new_SGPA,new_branch))
+			cur.execute(''' INSERT INTO Results (Rollnumber,Names,SGPA,Branch,CarryPapers) VALUES (?,?,?,?,?) ''', (new_roll_number,new_name,new_SGPA,new_branch,new_carry_papers))
 			print('Downloading data of roll number', i)
-			conn.commit()
+	
+	conn.commit()
 
 print("Enter the name of the file on which you want to save this data ... ")
 database = input()
@@ -68,7 +59,7 @@ database = database+'.sqlite'  #name of the database
 conn = sqlite3.connect(database)
 cur = conn.cursor()
 cur.execute(''' DROP TABLE IF EXISTS Results ''' )
-cur.execute(''' CREATE TABLE Results (Rollnumber INTEGER ,Names TEXT, SGPA INTEGER , Branch TEXT)  ''')
+cur.execute(''' CREATE TABLE Results (Rollnumber INTEGER ,Names TEXT, SGPA INTEGER , Branch TEXT, CarryPapers TEXT)  ''')
 
 print("Enter the year code... (ie. First two digits of your roll number) ")
 year_code = int(input())
@@ -77,9 +68,6 @@ branch_Code = int(input())
 
 first_roll = year_code*100000000 + 5200000 + branch_Code*1000 + 1
 last_roll = first_roll + 70 #at max
-
-test = get_page(first_roll)
-test = get_branch(test)
 
 
 lateral_first_roll = (year_code + 1)*100000000 + 5200000 + branch_Code*1000 + 9*100 + 1
